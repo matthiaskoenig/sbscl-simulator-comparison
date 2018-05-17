9b9c4b98e9753eef025b6bdea8f77a9a8af0a13a
Matthias Koenig <konigmatt@googlemail.com>
2018-05-17 18:42:23 +0200

    comparison of results

--- bigg_fba.py
+++ bigg_fba.py
@@ -4,6 +4,7 @@ Script for running FBA on all BiGG models.
 """
 import os
 import cobra
+import numpy as np
 import pandas as pd
 from cobra.io.sbml3 import read_sbml_model
 from cobra.util.solver import linear_reaction_coefficients
@@ -15,6 +16,7 @@ BIGG_MODELS_PATH = os.path.join(
     "models",
     BIGG_VERSION
 )
+EPS_COMPARISON = 1E-4
 
 
 def optimize_models(model_dir, results_path):
@@ -63,9 +65,33 @@ def optimize_models(model_dir, results_path):
     df.to_csv(results_path, sep="\t", index=False)
 
 
+def compare_results(cobrapy_path, sbscl_path, output_path):
+    """ Compare the FBA results.
+
+    :param cobrapy_file:
+    :param sbscl_file:
+    :return:
+    """
+    df_cobra = pd.read_csv(cobrapy_path, sep="\t")
+    df_cobra['mid'] = df_cobra.model.str.split('.').str[0]
+    df_sbscl = pd.read_csv(sbscl_path, sep="\t")
+    df_sbscl['mid'] = df_sbscl.model.str.split('.').str[0]
+    df = pd.merge(left=df_cobra, right=df_sbscl, on=['mid'])
+    df['identical'] = np.abs(df.objective_value_x - df.objective_value_y) < EPS_COMPARISON
+    print(df[df.identical == False])
+    df.to_csv(output_path, sep="\t", index=False)
+
+
 if __name__ == "__main__":
-    results_path = os.path.join(DIRECTORY, "bigg-fba-{}.tsv".format(BIGG_VERSION))
-    optimize_models(model_dir=BIGG_MODELS_PATH,
-                    results_path=results_path)
+    # simulation
+    cobrapy_path = os.path.join(DIRECTORY, 'results', "bigg-fba-{}_cobrapy.tsv".format(BIGG_VERSION))
+    optimize_models(model_dir=BIGG_MODELS_PATH, results_path=results_path)
+
+    # comparison of results
+    sbscl_path = os.path.join(DIRECTORY, 'results', "bigg-fba-{}_sbscl.tsv".format(BIGG_VERSION))
+    comparison_path = os.path.join(DIRECTORY, 'results', "bigg-fba-{}_comparison.tsv".format(BIGG_VERSION))
+    compare_results(cobrapy_path=cobrapy_path, sbscl_path=sbscl_path, output_path=comparison_path)
+
+
 
 
